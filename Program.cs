afc47dfd5526be4fef0df5627c23141249e9cec0
Boikata05 <gagariphi@gmail.com>
2024-03-11 18:05:59 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tedddd");
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+    }
+}
